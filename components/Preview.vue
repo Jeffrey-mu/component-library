d07073a2237662efe93e220b37145cdea52338cd
Jeffrey <1115378579@qq.com>
2023-05-26 16:43:00 +0800

    chore: 测试

--- components/Preview.vue
+++ components/Preview.vue
@@ -16,7 +16,6 @@ import sizes from '/data/screen-sizes.json'
 const size = ref('Moto 4G')
 const width = computed(() => sizes[size.value][0])
 const height = computed(() => sizes[size.value][1])
-console.log(sizes);
 
 </script>
 <style lang="scss" scoped>
